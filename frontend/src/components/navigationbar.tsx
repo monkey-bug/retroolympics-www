058a37b79dfea6308dffba3a49d7d31e2b07b1f7
DoodleyJC <dijkstradesley@gmail.com>
2024-07-16 01:24:59 +0200

    canged a to link tags

--- frontend/src/components/navigationbar.tsx
+++ frontend/src/components/navigationbar.tsx
@@ -1,30 +1,30 @@
-
+import Link from "next/link";
 
 export default function Navbar(){
     return (
     <div className="w-full h-64">
         <nav>
-            <ul className="flex justify-between text-2xl">
+            <ul className="flex justify-between text-2xl bg-purple-500">
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Home</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Home</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Teams</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Teams</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Standings</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Standings</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Schedules</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Schedules</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Games</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Games</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">Volunteers</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">Volunteers</Link>
                 </li>
                 <li className="mr-6">
-                    <a className="text-blue-500 hover:text-blue-800" href="/">History</a>
+                    <Link className="text-blue-500 hover:text-blue-800" href="/">History</Link>
                 </li>
             </ul>    
         </nav>      
