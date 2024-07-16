db2ecc127fa5687c2e6958284f8e28ab8bd5553f
DoodleyJC <dijkstradesley@gmail.com>
2024-07-16 15:59:57 +0200

    made it so login page actually works lol

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,24 +1,22 @@
 import { FormEvent } from 'react'
-import { useRouter } from 'next/router'
 import { cookies } from 'next/headers'
  
 export default function LoginPage() {
-  const router = useRouter()
  
   async function handleSubmit(formData: FormData) {
-
+    'use server'
  
     const email = formData.get('email')
     const password = formData.get('password')
  
 
  
-    cookies().set("isLoggedIn", "1", {expires: Date.now() + 1000*60 })
+    cookies().set("isLoggedIn", "1", {expires: Date.now() + 1000*60*60 }) //cookie that expires after 1 hour
   }
  
   return (
     <form action={handleSubmit}>
-      <input type="email" name="email" placeholder="Email" required />
+      <input type="text" name="email" placeholder="Email" required />
       <input type="password" name="password" placeholder="Password" required />
       <button type="submit">Login</button>
     </form>
