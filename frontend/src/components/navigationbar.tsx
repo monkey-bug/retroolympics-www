aa68f1d660efdb2f1d274003b55c2e16b0e8cc93
DoodleyJC <dijkstradesley@gmail.com>
2024-07-21 16:02:14 +0200

    added mx and text colour to parent component (ul)

--- frontend/src/components/navigationbar.tsx
+++ frontend/src/components/navigationbar.tsx
@@ -4,27 +4,27 @@ export default function Navbar(){
     return (
     <div className="w-full bg-gray-900">
         <nav>
-            <ul className="flex h-16 justify-between items-center text-2x">
-                <li className="mx-6">
-                    <Link className="text-blue-500 hover:text-blue-1000" href="/">Home</Link>
+            <ul className="flex h-16 justify-between items-center text-2x mx-20 text-blue-500">
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Home</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">Teams</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Teams</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">Standings</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Standings</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">Schedules</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Schedules</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">Games</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Games</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">Volunteers</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">Volunteers</Link>
                 </li>
-                <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800" href="/">History</Link>
+                <li>
+                    <Link className="hover:text-blue-800" href="/">History</Link>
                 </li>
             </ul>
         </nav>      
