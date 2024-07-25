df236860311b5530730453c1341c569d156843d4
DoodleyJC <dijkstradesley@gmail.com>
2024-07-25 14:29:05 +0200

    changed navigation bar and added betting link

--- frontend/src/components/navigationbar.tsx
+++ frontend/src/components/navigationbar.tsx
@@ -4,39 +4,44 @@ export default function Navbar() {
   return (
     <div className="w-full bg-gray-900">
       <nav>
-        <ul className="text-2x mx-20 flex h-16 items-center justify-between text-blue-500">
+        <ul className="text-2xl flex h-16 items-center justify-between text-blue-500">
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               Home
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded  bg-gray-600" href="/">
               Teams
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               Standings
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               Schedules
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               Games
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/betting">
+              Betting
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               Volunteers
             </Link>
           </li>
           <li>
-            <Link className="hover:text-blue-800" href="/">
+            <Link className="hover:bg-gray-700 px-2 py-1 rounded bg-gray-600" href="/">
               History
             </Link>
           </li>
