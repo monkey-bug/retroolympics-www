e89f6c09f9d7a9e5b6ca4c343ff0a1a3b793a572
DoodleyJC <dijkstradesley@gmail.com>
2024-07-25 14:30:37 +0200

    fixed typo

--- frontend/src/app/betting/page.tsx
+++ frontend/src/app/betting/page.tsx
@@ -6,8 +6,8 @@ export default function BettingPage(){
 
     const matchesList = matches.map((element) => {
         const undefinedTeam: Team = { "id": -1, "name" : "undefined team" }
-        var teamObject1 = teams.find((team) => team.id==element.teams[0]);
-        var teamObject2 = teams.find((team) => team.id==element.teams[1]);
+        var teamObject1 = teams.find((team) => team.id==element.teams[0].id);
+        var teamObject2 = teams.find((team) => team.id==element.teams[1].id);
         if(teamObject1 === undefined){
             teamObject1 = undefinedTeam; 
         }
